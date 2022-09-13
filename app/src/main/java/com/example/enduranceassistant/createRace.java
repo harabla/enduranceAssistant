fe64f837fb06bfce148275aeb8ec1293209f1796
harabla <blankenstein.harri@gmail.com>
2022-09-13 13:42:11 +0300

    - Drawn first events to get a fuller picture. Next up - Live racing event.

---
+++ app/src/main/java/com/example/enduranceassistant/createRace.java
@@ -0,0 +1,14 @@
+package com.example.enduranceassistant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class createRace extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_race);
+    }
+}
